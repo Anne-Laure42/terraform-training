4d25a60b00c6630e3e94d6e46c41c0dac3205366
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-09-07 12:40:06 +0200

    Update provider.tf

--- TP sur Azure/TP2 - Premier déploiement/provider.tf
+++ TP sur Azure/TP2 - Premier déploiement/provider.tf
@@ -1,8 +1,9 @@
 terraform {
- required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-  }
+   required_version = ">= 0.12"
+   required_providers {
+      azurerm = ">3.0"
+   }
+} 
+provider "azurerm" {
+  features {}
+}
