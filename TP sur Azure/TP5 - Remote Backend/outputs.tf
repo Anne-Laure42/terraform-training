f8c1aaf4f3583e3169eb7d9b4c2d656e66ba2b32
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-10 16:20:31 +0200

    Create outputs.tf

---
+++ TP sur Azure/TP5 - Remote Backend/outputs.tf
@@ -0,0 +1,13 @@
+output "public_ip" {
+  value = "${azurerm_linux_virtual_machine.tfeazytraining-nginxserver.name}: ${data.azurerm_public_ip.tfeazytraining-pubip.ip_address}"
+}
+
+output "private_key" {
+  value = tls_private_key.ssh.private_key_openssh
+  sensitive=true
+}
+
+output "public_key" {
+  value = tls_private_key.ssh.private_key_openssh
+  sensitive=true
+}
