78a566d22f996592cd050f631e39fa52c1f3b9b6
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-10 16:20:49 +0200

    Update provider.tf

--- TP sur Azure/TP5 - Remote Backend/provider.tf
+++ TP sur Azure/TP5 - Remote Backend/provider.tf
@@ -1,16 +1,13 @@
 terraform {
- required_providers {
+  required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "=3.59.0"
     }
-  }
-  backend "azurerm" {
-    resource_group_name  = "my-terraform-rg"
-    storage_account_name = "azure-backend-<votre prénom>"
-    container_name       = "backend"
-    key                  = "terraform.tfstate"
   }
 }
 
-
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
