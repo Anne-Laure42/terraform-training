3f0717ae88976734f3f65ceb3fed9a32d374dd66
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-10 15:28:46 +0200

    Update data.tf

--- TP sur Azure/TP5 - Remote Backend/data.tf
+++ TP sur Azure/TP5 - Remote Backend/data.tf
@@ -1,6 +1,11 @@
 data "azurerm_platform_image" "eazytraining-image" {
-  location  = "West Europe"
+  location  = "france central"
   publisher = "Canonical"
   offer     = "0001-com-ubuntu-server-focal"
-  sku       = "18_04-lts"
+  sku       = "20_04-lts"
+}
+
+data "azurerm_public_ip" "tfeazytraining-pubip" {
+  name                = azurerm_public_ip.tfeazytraining-pubip.name
+  resource_group_name = azurerm_resource_group.tfeazytraining-gp.name
 }
