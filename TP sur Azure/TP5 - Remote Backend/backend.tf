6c5ea5ab0db0fa318c551ebe3d47a12015c88823
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-12 10:27:31 +0200

    Update backend.tf

--- TP sur Azure/TP5 - Remote Backend/backend.tf
+++ TP sur Azure/TP5 - Remote Backend/backend.tf
@@ -1,8 +1,9 @@
 terraform {
     backend "azurerm" {
-      resource_group_name  =  var.resource_group_name
-      storage_account_name = "tfstate<RANDOM_STRING>"
+      resource_group_name  = "tfeazytraining-gp"
+      storage_account_name = "tfstatepzcgbwu5"
       container_name       = "my-blob-tfstate"
-      key                  = "terraform.tfstate"
+      access_key           = "AHyGCVVEbMwS1TyQ/nL73FDWxNtB1VtEI6o1yWlQD3AdKIoaaheu/oKbvEDYs32RLTZy4RjGbpkV+AStttG8Ng=="
+      key                  = "test/terraform.tfstate"
   }
 }
