0b6ef85b6b9717bcf3579adcd3f5c679449f99e2
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-20 12:31:52 +0200

    Update main.tf

--- TP sur Azure/TP-12 For each/main.tf
+++ TP sur Azure/TP-12 For each/main.tf
@@ -1,1 +1,39 @@
-sd
+resource "azurerm_resource_group" "example" {
+  name     = "${local.resource_name}-rg"
+  location = lookup(var.allowed_location,var.location, null)
+
+   tags = local.required_tags
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "${local.resource_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+   tags = local.required_tags
+}
+
+resource "azurerm_subnet" "example" {
+  for_each             = local.subnets
+  name                 = each.value.name
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = each.value.address_prefixes
+
+}
+
+resource "azurerm_network_security_group" "example" {
+  for_each            = local.nsgs
+  name                = each.value.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+ 
+  tags = local.required_tags
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  for_each                  = azurerm_subnet.example
+  subnet_id                 = each.value.id
+  network_security_group_id = azurerm_network_security_group.example[lookup(local.subnets, each.value.name, null).nsg].id
+}
