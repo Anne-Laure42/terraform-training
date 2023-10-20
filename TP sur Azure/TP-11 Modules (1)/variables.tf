de58ba1543b79ab2537822989d7a9352dcbe5da7
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-20 10:41:18 +0200

    Create variables.tf

---
+++ TP sur Azure/TP-11 Modules (1)/variables.tf
@@ -0,0 +1,40 @@
+variable "environment" {
+    type = string
+    description = "environement"
+    default = "test"
+}
+
+#variable "name" {
+#   description = "(optional) describe your variable"
+#    default = module.azurerm_storage_account.sto.name
+#}
+
+variable "resource_group_name" {
+    type = string
+    description = "Name of the rg"
+    default = "test"
+}
+
+#variable "location" {
+#    type = string
+#    description = "Location of the storage account"
+#    default = "france central"
+#}
+
+#variable "account_kind" {
+#    type = string
+#    description = "The Kind of account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2"
+#    default = "BlobStorage"
+#}
+
+#variable "account_tier" {
+#    type = string
+#    description = "Define the Tier to use for this storage account (Standard or Premium)"
+#    default = "Standard"
+# }
+
+#variable "replication_type" {
+#    type = string
+#    description = "Storage account replication type - i.e. LRS, GRS, RAGRS, ZRS, GZRS, RAGZRS"
+#    default = "LRS"
+#}
