5bc49557328286cce8f7de83b94fba46b850bc40
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-20 10:26:46 +0200

    Create provider.tf

---
+++ TP sur Azure/TP-11 Count/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.59.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  resource_name = "test"
+  required_tags = "${var.project}"
+}
