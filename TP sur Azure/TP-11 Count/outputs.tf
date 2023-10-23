6cc3a9210007bd975a97ecccf5fdaa373a7c4fe8
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-10-23 10:50:53 +0200

    Update outputs.tf

--- TP sur Azure/TP-11 Count/outputs.tf
+++ TP sur Azure/TP-11 Count/outputs.tf
@@ -1,3 +1,7 @@
-output "public_ip" {
-  value = "azurerm_linux_virtual_machine.example.name"
+output "all_nic_id" {
+  value = azurerm_network_interface.example[*].id
+}
+
+output "one_nic_id" {
+  value = azurerm_network_interface.example[1].id
 }
