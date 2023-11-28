100679df8ebc83b3a3b70ab44b0bee1d12089742
Anne-Laure42 <123458466+Anne-Laure42@users.noreply.github.com>
2023-11-28 16:06:37 +0100

    Create variables.tf

---
+++ TP sur Azure/TP16 - Multiples subnets with VMs/variables.tf
@@ -0,0 +1,51 @@
+locals {
+  resource_name = "anne-testlab"
+
+  required_tags = "${var.project}"
+}
+
+variable "cidr_blocks" {
+  default = "10.0.0.0/24"
+}
+
+variable "subnet_prefix" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "subnet-"
+}
+
+variable "location" {
+    type = string
+    description = "Location of the resource group"
+    default = "fr"
+}
+
+variable "allowed_location" {
+    type = map
+    description = "Location allowed for the rg"
+    default = {
+        fr = "france central"
+        neu = "north europe"
+    }
+}
+
+variable "project" {
+    type = map(string)
+    description = "(optional) describe your variable"
+    default = {
+        environement = "demo"
+        project_name = "lab"
+    }
+}
+
+variable "vm_count" {
+    type = string
+    description = "(optional) describe your variable"
+    default = 3
+}
+
+variable "vm_name_prefix" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "vm-"
+}
